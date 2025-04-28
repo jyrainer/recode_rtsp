df71ea51eb2ef8130111cd983de3ae3c608220a1
박재용 <jy.park@pia.space>
2025-04-28 10:26:59 +0900

    feat: add setup.py for package configuration

---
+++ setup.py
@@ -0,0 +1,16 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='recode-rtsp',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python',
+    ],
+    python_requires='>=3.9',
+    author='Jaeyong Park',
+    author_email='jy.park@pia.space',
+    description='Record RTSP streams and save them as MP4 files.',
+    url='https://github.com/jyrainer/recode_rtsp.git',
+)
