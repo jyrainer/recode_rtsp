09fbe4938c4356b112a34a8b4b57a47ec78def04
박재용 <jy.park@pia.space>
2025-04-28 21:01:04 +0900

    feat: add main script for RTSP stream recording

---
+++ main.py
@@ -0,0 +1,8 @@
+from recode_rtsp.recode_rtsp import RecodeRtsp
+
+rtsp_streams = [
+    'rtsp://172.168.47.35:8555/minite_timer',
+    'rtsp://172.168.47.35:8555/tailgate_134'
+]
+recorder = RecodeRtsp.new(root_dir_saving="./assets")
+recorder.recode(rtsp_streams=rtsp_streams, recode_period=4, postfix='mp4', max_duration=14, merge=True)
