52b1a19915e77c96778fb9963404f408e01fac49
박재용 <jy.park@pia.space>
2025-04-28 22:21:08 +0900

    fix: 시간기반이 아닌 프레임수 기반, pre-commit 적용

--- recode_rtsp/utils/recode.py
+++ recode_rtsp/utils/recode.py
@@ -1,22 +1,24 @@
 import os
-import cv2
 import time
 from datetime import datetime
 
+import cv2
+
 from recode_rtsp.utils.trans_str import sanitize_folder_name
 
+
 def record_stream(stream_url, info, running_flag, max_duration):
-    video_postfix = info['postfix']
+    video_postfix = info["postfix"]
     cap = cv2.VideoCapture(stream_url)
     if not cap.isOpened():
         print(f"Failed to open stream: {stream_url}")
         return
 
     folder_name = sanitize_folder_name(stream_url)
-    save_folder = os.path.join(info['root_dir_saving'], folder_name)
+    save_folder = os.path.join(info["root_dir_saving"], folder_name)
     os.makedirs(save_folder, exist_ok=True)
 
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
 
     fps = cap.get(cv2.CAP_PROP_FPS)
     if fps <= 1 or fps != fps:  # 0 or NaN
@@ -27,8 +29,7 @@ def record_stream(stream_url, info, running_flag, max_duration):
 
     out = None
     frame_count = 0
-    total_start_time = time.time()
-    segment_start_time = total_start_time 
+    segment_frame_count = 0
 
     try:
         print(f"[INFO] Starting recording for {stream_url}")
@@ -39,27 +40,33 @@ def record_stream(stream_url, info, running_flag, max_duration):
                 break
 
             frame_count += 1
+            segment_frame_count += 1
 
             elapsed_video_time = frame_count / fps
+            segment_elapsed_time = segment_frame_count / fps
 
             if max_duration is not None and elapsed_video_time >= max_duration:
-                print(f"[INFO] Max duration {max_duration} seconds reached based on frame count. Stopping recording.")
+                print(
+                    f"[INFO] Max duration {max_duration} seconds reached based on frame count. Stopping recording."
+                )
                 break
 
             current_time = time.time()
 
-            if out is None or (current_time - segment_start_time) >= info['recode_period']:
+            if out is None or segment_elapsed_time >= info["recode_period"]:
                 if out is not None:
                     out.release()
-                filename = datetime.now().strftime('%Y%m%d_%H%M%S') + f'.{video_postfix}'
+                filename = datetime.now().strftime("%Y%m%d_%H%M%S") + f".{video_postfix}"
                 filepath = os.path.join(save_folder, filename)
                 out = cv2.VideoWriter(filepath, fourcc, fps, (width, height))
-                segment_start_time = current_time
+                segment_frame_count = 0
 
             if out:
                 out.write(frame)
+
     except KeyboardInterrupt:
         print("KeyboardInterrupt detected, saving file...")
+
     finally:
         if out:
             out.release()
